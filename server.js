d343b57d6c8e3a56b6a5d3a12bf86205ae48f427
Jesus Medero <jmedero09@gmail.com>
2017-03-16 03:16:29 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+//express.static is going to take the absoulte path to the file that we want to serve up
+//__dirname gives us the path to the folder node-web-server then we concatnate /public to give it access to the public folder
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = now + req.method + req.url
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('unable to append to server.log');
+        }
+    });
+    next();
+});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+//will let us set up a handler for an http get request
+//this is our root route
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcome: 'Hey there my friend'
+    });
+});
+
+//this is our about route
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
+});
+
+//this out bad route
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'this is a horrible request',
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
